5e4d2c55008979821869eaa60eef93ead74f8cf1
Rioba-Ian <riobafelix@gmail.com>
2025-07-06 20:05:45 +0300

    stream users in batches and display them

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,30 @@
+"""
+Write a function stream_users_in_batches(batch_size) that fetches rows in batches
+
+Write a function batch_processing() that processes each batch to filter users over the age of25`
+"""
+
+import mysql.connector
+
+
+def stream_users_in_batches(batch_size):
+    connection = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="password",
+      database="mydatabase"
+    )
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM users")
+        while True:
+            rows = cursor.fetchmany(batch_size)
+            if not rows:
+                break
+            yield rows
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        cursor.close()
+        connection.close()
