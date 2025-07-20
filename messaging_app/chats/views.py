1be3f525272f38f517626da2a068e2c8f8b47c16
Rioba-Ian <riobafelix@gmail.com>
2025-07-20 17:58:51 +0300

    create viewsets for messages

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CustomUser, Conversation, Message
+from .serializers import CustomUserSerializer, ConversationSerializer, MessageSerializer
 
 # Create your views here.
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(sender_id=self.request.user)
