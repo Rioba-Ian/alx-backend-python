b7cfc9bca70b9a414815b2d03cfab87556a254e4
Rioba-Ian <riobafelix@gmail.com>
2025-08-01 16:57:13 +0300

    register models in admin.py

--- Django-Middleware-0x04/chats/admin.py
+++ Django-Middleware-0x04/chats/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Message, Conversation, Notification
 
 # Register your models here.
+
+
+admin.site.register(Message)
+admin.site.register(Conversation)
+admin.site.register(Notification)
